0f00132c4bdc37ee3ef02adcb0abd5b9e932710d
Kaede Dev Kento Hinode <139364723+DarsheeeGamer@users.noreply.github.com>
2024-09-22 18:37:24 +0530

    Add files via upload

---
+++ aarch64-apple-darwin/release/deps/itoa-c792419a8bf42865.d
@@ -0,0 +1,8 @@
+/Users/renukrishna/Downloads/kaede-extension-maker-mac/target/aarch64-apple-darwin/release/deps/libitoa-c792419a8bf42865.rmeta: /Users/renukrishna/.cargo/registry/src/index.crates.io-6f17d22bba15001f/itoa-1.0.11/src/lib.rs /Users/renukrishna/.cargo/registry/src/index.crates.io-6f17d22bba15001f/itoa-1.0.11/src/udiv128.rs
+
+/Users/renukrishna/Downloads/kaede-extension-maker-mac/target/aarch64-apple-darwin/release/deps/libitoa-c792419a8bf42865.rlib: /Users/renukrishna/.cargo/registry/src/index.crates.io-6f17d22bba15001f/itoa-1.0.11/src/lib.rs /Users/renukrishna/.cargo/registry/src/index.crates.io-6f17d22bba15001f/itoa-1.0.11/src/udiv128.rs
+
+/Users/renukrishna/Downloads/kaede-extension-maker-mac/target/aarch64-apple-darwin/release/deps/itoa-c792419a8bf42865.d: /Users/renukrishna/.cargo/registry/src/index.crates.io-6f17d22bba15001f/itoa-1.0.11/src/lib.rs /Users/renukrishna/.cargo/registry/src/index.crates.io-6f17d22bba15001f/itoa-1.0.11/src/udiv128.rs
+
+/Users/renukrishna/.cargo/registry/src/index.crates.io-6f17d22bba15001f/itoa-1.0.11/src/lib.rs:
+/Users/renukrishna/.cargo/registry/src/index.crates.io-6f17d22bba15001f/itoa-1.0.11/src/udiv128.rs:
---
+++ aarch64-apple-darwin/release/deps/kaede_extension_maker_mac-c00010421871cd17.d
@@ -0,0 +1,5 @@
+/Users/renukrishna/Downloads/kaede-extension-maker-mac/target/aarch64-apple-darwin/release/deps/kaede_extension_maker_mac-c00010421871cd17: src/main.rs
+
+/Users/renukrishna/Downloads/kaede-extension-maker-mac/target/aarch64-apple-darwin/release/deps/kaede_extension_maker_mac-c00010421871cd17.d: src/main.rs
+
+src/main.rs:
